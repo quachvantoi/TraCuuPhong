e394529493a8d74bbf46729a0558cc73d5d4b293
quachvantoi <80662163+quachvantoi@users.noreply.github.com>
2022-07-03 16:16:14 +0700

    Upload web Tra Cứu giá phòng
    Upload web Tra Cứu giá phòng

--- ketnoi.php
+++ ketnoi.php
@@ -1,7 +1,6 @@
 <?php
-    $connect= mysqli_connect("localhost","id18703555_toi1234","g2ylbROWf(qnIR!y");
-    // mysqli_select_db($connect, "DOAN1");
-    mysqli_select_db($connect, "id18703555_doantotnghiep");
+    $connect= mysqli_connect("localhost","username","pass");
+    mysqli_select_db($connect, "database");
     mysqli_query($connect, "SET names 'utf8'");
     if(!$connect){
       echo "Không thể kết nối đến Database!".mysqli_connect_error($connect);
